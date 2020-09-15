755262aae173fc5399de13ef5a4c18207dba8272
noname9xnd <noname9xnd@gmail.com>
2020-09-16 00:33:53 +0700

    test template

---
+++ SMSOnline/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSOnline.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult FindFriend()
+        {
+            return View();
+        }
+
+        public ActionResult InfoUser()
+        {
+            return View();
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public ActionResult ListFriendAndContact()
+        {
+            return View();
+        }
+    }
+}
