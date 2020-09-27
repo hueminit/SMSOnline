307b261806dc48d846468061b703706d6a82944f
Chung Handsome <chungpham0412@gmail.com>
2020-09-27 17:23:20 +0700

    update config mail

--- Models/ViewModel/Others/MailSettingModel.cs
+++ Models/ViewModel/Others/MailSettingModel.cs
@@ -5,8 +5,8 @@
         public string Server { set; get; } = "smtp.gmail.com";
         public int Port { set; get; } = 587;
         public bool EnableSsl { set; get; } = true;
-        public string UserName { set; get; } = "flatizeshop@gmail.com";
-        public string Password { set; get; } = "flatize123";
+        public string UserName { set; get; } = "smsonline.bkap@gmail.com";
+        public string Password { set; get; } = "smsonline";
         public string FromEmail { set; get; } = "SMSOnline@gmail.com";
         public string FromName { set; get; } = "SMSOnline Admin";
     }
